5eddb612c726a08d47497da6e246a14c0cd9d2be
Javier Vargas <ijavom@gmail.com>
2023-06-28 21:06:01 -0500

    first commit

---
+++ clase/01-transpilacion.ts
@@ -0,0 +1,13 @@
+class HolaMundo{
+    varAtributo:string = "";
+    varNumber:number = 0;
+}
+
+(()=>{
+    1>0 ? 'hola':'adios';
+    let variableNumerica : number;
+    variableNumerica = 1 - 1;
+    variableNumerica++;
+    console.log('Hice un cambio');
+})()
+
---
+++ dist/01-transpilacion.js
@@ -0,0 +1,14 @@
+"use strict";
+class HolaMundo {
+    constructor() {
+        this.varAtributo = "";
+        this.varNumber = 0;
+    }
+}
+(() => {
+    1 > 0 ? 'hola' : 'adios';
+    let variableNumerica;
+    variableNumerica = 1 - 1;
+    variableNumerica++;
+    console.log('Hice un cambio');
+})();
